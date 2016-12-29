5d815db70dc86b2ec1de1c5ea9c51d47d15f1cb2
Michael Chaffins <michaelchaffins7@gmail.com>
2016-12-28 20:28:02 -0700

    jQuery changes

--- ol3_wfs_t.js
+++ ol3_wfs_t.js
@@ -1,8 +1,10 @@
 //const epsg = '3857';
 const epsg = '4326';
-const namespace = 'test.issinc.com';
-const workspace = 'Test';
-const databaseName = 'Test';
+const namespaceURI = 'michael.com';
+const workspace = 'michael';
+const databaseName = 'postgres';
+const layerGroup = 'michael_layers';
+const dataStore = 'openlayers';
 
 var featureLayerName;
 var interaction;
@@ -10,78 +12,92 @@ var format;
 var inputProjection;
 var outputProjection;
 
-function getLayers(url){
-    var httpRequest = new XMLHttpRequest(); 
-    httpRequest.open("GET", url, false);
-    httpRequest.send(null);
-    return httpRequest.responseText;          
-}
-
-function displayLayers() {
-    var json_obj = JSON.parse(getLayers('http://localhost:8080/geoserver/rest/workspaces/' + workspace + '/featuretypes.json'));
-    var featureTypes = json_obj.featureTypes;
-    if (featureTypes.length < 1) {
-        $("#layerListDiv").append(s);
-        return;
-    }
-
-    var layerList = [];
-    for (var i = 0; i < json_obj.featureTypes.featureType.length; i++)
-        layerList.push(json_obj.featureTypes.featureType[i].name);
-
-    for (var j = 0; j < layerList.length; j++) {
-        var layerName = layerList[j];
-        var r = $('<input type="button" class="layerButton" value="' 
-                + layerName 
-                + '" onclick="refreshLayer(\'' + layerName + '\');"/><br><br>');
-        $("#layerListDiv").append(r);    
-    }
-    var s = $('<input type="button" id="addButton" value="Add Layer" onclick="addNewLayer();"/><br>');
-    $("#layerListDiv").append(s);
-}
-displayLayers();
-
-function getLayerXML(newLayerName) {
-    return "<featureType>"
-          +   "<name>" + newLayerName + "</name>"
-          +   "<nativeName>" + newLayerName + "</nativeName>"
-          +   "<title>" + newLayerName + "</title>"
-          +   "<srs>EPSG:" + epsg + "</srs>"
-          +   "<attributes>"
-          +     "<attribute>"
-          +       "<name>geometry</name>"
-          +       "<binding>com.vividsolutions.jts.geom.Geometry</binding>"
-          +     "</attribute>"
-          +   "<attribute>"
-          +      "<name>Name</name>"
-          +      "<binding>java.lang.String</binding>"
-          +    "</attribute>"
-          +    "</attributes>"
-          +"</featureType>";
-} 
-
-function addNewLayer() {
-    var newLayerName = prompt("Enter new layer name:");
-    if (newLayerName === null) {
-        return;
-    }
-
-    var layerName = getLayerXML(newLayerName);
+// JSONP WMS call for list of layers
+function getLayerList() {
     $.ajax({
-        url: 'http://localhost:8080/geoserver/rest/workspaces/' + workspace + '/datastores/' + databaseName + '/featuretypes',
-        type: "POST",
-        contentType: "text/xml",
-        data: layerName, 
-        srsName: 'EPSG:4326'
-    }) .done(function() {
-            $("#addButton").remove();
-            $('br:last-child').remove();
-            $('br:last-child').remove();
-            var r = $('<br><input type="button" class="layerButton" value="' + newLayerName + '" onclick="refreshLayer(\'' + newLayerName + '\');"/><br><br>');
-            $("#layerListDiv").append(r);
-            var s = $('<input type="button" id="addButton" value="Add Layer" onclick="addNewLayer();"/><br>');
-            $("#layerListDiv").append(s); 
+        type: 'GET',
+        url: 'http://localhost:8080/geoserver/' + workspace + '/wfs?' +
+        'SERVICE=WFS&VERSION=2.0.0&' +
+        'REQUEST=DescribeFeatureType&' +
+        'outputFormat=text/javascript',
+        dataType: 'jsonp',
     });
+}
+getLayerList();
+
+// callback function of WMS call that lists the layers to select
+function parseResponse(jsonp) {
+    var $addButton = $('<input type="button" id="addButton" value="Add Layer" onclick="addNewLayer();"/><br>');
+
+    // exit function if no layers have been added
+    if (jsonp.featureTypes.length < 1) {
+        $("#layerListDiv").append($addButton);
+        return;
+    }
+
+    // loop through json object and add button for each layer
+    $.each(jsonp.featureTypes, function (i, item) {
+        var layerName = item.typeName;
+        $("#layerListDiv").append($('<input type="button" class="layerButton" value="' +
+            layerName +
+            '" onclick="refreshLayer(\'' + layerName + '\');"/><br><br>'));
+    });
+
+    $("#layerListDiv").append($addButton);
+}
+
+// create and add layer to database
+function addNewLayer() {
+    var $addButton = $('#addButton');
+    var $layerForm = $('<form id="layerForm">Enter new layer name: <input type="text" name="newLayerName" id="newLayerName"><br>' +
+        '<button type="button" id="cancel">Cancel</button>' +
+        '<button type="button" id="submit">Submit</button></form>');
+    $addButton.before($layerForm);
+    $layerForm.append($addButton.children());
+    $addButton.hide();
+
+    $('#cancel').click(function () {
+        $layerForm.remove();
+        $addButton.show();
+    });
+
+    $('#submit').click(function () {
+        var $newLayerName = $('#newLayerName').val();
+        var $newLayerButton = $('<input type="button" class="layerButton" value="' + $newLayerName + '" onclick="refreshLayer(\'' + $newLayerName + '\');"/><br><br>');
+        var layerXML = getLayerXML($newLayerName);
+
+        $.ajax({
+            url: 'http://localhost:8080/geoserver/rest/workspaces/' + workspace + '/datastores/' + dataStore + '/featuretypes',
+            type: "POST",
+            contentType: "text/xml",
+            data: layerXML,
+            srsName: 'EPSG:' + epsg
+        }).done(function () {
+            $layerForm.remove();
+            $newLayerButton.insertBefore($addButton);
+            $addButton.show();
+        });
+    });
+}
+
+// XML string to create a table in the database for a new layer
+function getLayerXML(newLayerName) {
+    return "<featureType>" +
+        "<name>" + newLayerName + "</name>" +
+        "<nativeName>" + newLayerName + "</nativeName>" +
+        "<title>" + newLayerName + "</title>" +
+        "<srs>EPSG:" + epsg + "</srs>" +
+        "<attributes>" +
+        "<attribute>" +
+        "<name>geometry</name>" +
+        "<binding>com.vividsolutions.jts.geom.Geometry</binding>" +
+        "</attribute>" +
+        "<attribute>" +
+        "<name>Name</name>" +
+        "<binding>java.lang.String</binding>" +
+        "</attribute>" +
+        "</attributes>" +
+        "</featureType>";
 }
 
 var interactionSelectPointerMove = new ol.interaction.Select({
@@ -109,29 +125,30 @@ var interactionSelect = new ol.interaction.Select({
 var interactionSnap;
 var layerWFS = new ol.layer.Vector();
 var sourceWFS;
+
 function refreshLayer(layerName) {
     $("#currentLayer").html('');
     $("#currentLayer").append(layerName);
     featureLayerName = layerName;
     layer = layerName;
-    sourceWFS = new ol.source.Vector({  
-    loader: function(extent) {
-        $.ajax('http://localhost:8080/geoserver/wfs', {
-            type: 'GET',
-            data: {
-                service: 'wfs',
-                version: '1.1.0',
-                request: 'GetFeature',
-                typename: 'Test:' + layer,
-                srsname: 'EPSG:' + epsg,
-                bbox: extent.join(',') + ',EPSG:' + epsg,
-            }
-        }).done(function(response) {
-            sourceWFS.addFeatures(formatWFS.readFeatures(response));
-        });
-    },
-    projection: 'EPSG:' + epsg,
-    strategy: ol.loadingstrategy.bbox
+    sourceWFS = new ol.source.Vector({
+        loader: function (extent) {
+            $.ajax('http://localhost:8080/geoserver/wfs', {
+                type: 'GET',
+                data: {
+                    service: 'wfs',
+                    version: '1.1.0',
+                    request: 'GetFeature',
+                    typename: workspace + ':' + layer,
+                    srsname: 'EPSG:' + epsg,
+                    bbox: extent.join(',') + ',EPSG:' + epsg,
+                }
+            }).done(function (response) {
+                sourceWFS.addFeatures(formatWFS.readFeatures(response));
+            });
+        },
+        projection: 'EPSG:' + epsg,
+        strategy: ol.loadingstrategy.bbox
     });
     layerWFS.setSource(sourceWFS);
     map.addLayer(layerWFS);
@@ -148,7 +165,7 @@ function updateFormats() {
     var formats = {
         geojson: new ol.format.GeoJSON(),
         kml: new ol.format.KML(),
-        wkt: new ol.format.WKT(),       
+        wkt: new ol.format.WKT(),
         gpx: new ol.format.GPX(),
     };
     format = formats[formatType];
@@ -175,7 +192,7 @@ var map = new ol.Map({
         center: [0, 0],
         zoom: 3,
     })
-}); 
+});
 var scaleLine = new ol.control.ScaleLine();
 map.addControl(scaleLine);
 var zoomControl = new ol.control.Zoom();
@@ -186,7 +203,7 @@ var formatGML;
 var dirty = {};
 var transactWFS = function (mode, f) {
     formatGML = new ol.format.GML({
-        featureNS: namespace,
+        featureNS: namespaceURI,
         featureType: featureLayerName,
         srsName: 'EPSG:4326',
     });
@@ -209,7 +226,7 @@ var transactWFS = function (mode, f) {
         processData: false,
         contentType: 'text/xml',
         data: payload,
-    }).done(function() {
+    }).done(function () {
         sourceWFS.clear();
     });
 };
@@ -276,7 +293,7 @@ $('button').click(function () {
             map.addInteraction(interaction);
             interaction.on('drawend', function (e) {
                 var name = prompt("Name:");
-                e.feature.set('Name', name);                
+                e.feature.set('Name', name);
                 transactWFS('insert', e.feature);
             });
             break;
@@ -324,11 +341,12 @@ $('button').click(function () {
  */
 
 var mousePosition;
+
 function updateMouseProjection() {
     if (mousePosition != null) {
         map.removeControl(mousePosition);
     }
-    var mousePositionProjection = document.getElementById("mouseProjection").value;
+    var mousePositionProjection = $('#mouseProjection').val();
     mousePosition = new ol.control.MousePosition({
         coordinateFormat: ol.coordinate.createStringXY(2),
         projection: mousePositionProjection
@@ -338,12 +356,12 @@ function updateMouseProjection() {
 updateMouseProjection();
 
 function updateInputProjection() {
-    inputProjection = document.getElementById("inputProjection").value;
+    inputProjection = $('#inputProjection').val();
 }
 updateInputProjection();
 
 function updateOutputProjection() {
-    outputProjection = document.getElementById("outputProjection").value;
+    outputProjection = $('#outputProjection').val();
 }
 updateOutputProjection();
 
@@ -351,16 +369,16 @@ updateOutputProjection();
  * CHANGE PROJECTION FORMAT
 
 
-function to4326() {
+ function to4326() {
     var source = layerWFS.getSource();
     var features = source.getFeatures();
     features.forEach(function (featuresM) {
         var geometry = featuresM.getGeometry();
         geometry.transform('EPSG:3857', 'EPSG:4326');
-    }); 
+    });
 }
 
-function to3857() {
+ function to3857() {
     var source = layerWFS.getSource();
     var features = source.getFeatures();
     features.forEach(function (featuresM){
@@ -368,7 +386,7 @@ function to3857() {
         geometry.transform('EPSG:4326', 'EPSG:3857');
     });
 }
-*/
+ */
 /**
  * READ DATA FROM TEXTBOX
  */
@@ -378,7 +396,7 @@ function readData() {
     var source = layerWFS.getSource();
     source.clear();
     var data = document.getElementById("readTextArea").value;
-    if(inputProjection === "EPSG:4326") {
+    if (inputProjection === "EPSG:4326") {
         to4326();
         featuresRead = format.readFeatures(data);
         source.addFeatures(featuresRead);
@@ -389,34 +407,34 @@ function readData() {
     }
 }
 
-/** 
+/**
  * WRITE DATA TO TEXTBOX
-*/
+ */
 
 function writeAllFeatures() {
     var data;
     var source = layerWFS.getSource();
     var features = source.getFeatures();
-    if(outputProjection === "EPSG:4326") {
+    if (outputProjection === "EPSG:4326") {
         //to4326();
         data = format.writeFeatures(features);
         document.getElementById("writeTextArea").innerHTML = data;
         to3857();
     } else {
         data = format.writeFeatures(features);
-        document.getElementById("writeTextArea").innerHTML = JSON.stringify(JSON.parse(data),null,2);;
+        document.getElementById("writeTextArea").innerHTML = JSON.stringify(JSON.parse(data), null, 2);;
     }
 }
 
 function writeSelectedFeature(selectedFeature) {
     var data;
-    if(outputProjection === "EPSG:4326") {
+    if (outputProjection === "EPSG:4326") {
         //to4326();
         data = format.writeFeature(selectedFeature);
         document.getElementById("writeTextArea").innerHTML = data;
         to3857();
     } else {
         data = format.writeFeature(selectedFeature);
-        document.getElementById("writeTextArea").innerHTML = JSON.stringify(JSON.parse(data),null,2);
+        document.getElementById("writeTextArea").innerHTML = JSON.stringify(JSON.parse(data), null, 2);
     }
 }
